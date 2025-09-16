a36fc7eef2303b9d2316a104326af2d11b2c14ee
Gabriel <gcolbath19@gmail.com>
2025-09-16 12:18:09 -0400

    Some changes to the LSP

--- config/lsp.lua
+++ config/lsp.lua
@@ -12,4 +12,29 @@ lspconfig.zls.setup({
     workspace_required = false,
 })
 
+local clangd_extra_capabilities = {
+    offsetEncoding = { "utf-8", "utf-16" },
+    textDocument = {
+        completion = {
+            editsNearCursor = true,
+        },
+    },
+}
+local clangd_capabilities = vim.tbl_deep_extend("force", clangd_extra_capabilities, capabilities)
+lspconfig.clangd.setup({
+    capabilities = clangd_capabilities,
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+    root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
+})
+
+lspconfig.pylsp.setup({
+    capabilities = capabilities,
+    cmd = { "pylsp" },
+    filetypes = { "python" },
+    root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+})
+
 vim.lsp.enable("zls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("pylsp")
